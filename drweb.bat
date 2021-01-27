a6432909a85fc725a18ac2aa4aa9db18ea7e37da
BabushkaVoytenko <serezha845765@gmail.com>
2021-01-27 19:48:10 +0300

    edite drweb

--- drweb.bat
+++ drweb.bat
@@ -1,9 +1,9 @@
 :: Данный скрипт удаляет предыдущую версию файла антивируса и качает новую версию сайта
 @echo off
-del /f /q d:\folder\setup.exe 
+del /f /q "d:\folder\setup.exe" 
 :: удаляем старую версию файла из папки куда он скачивается
-wget -P d:/folder https://download.geo.drweb.com/pub/drweb/cureit/setup.exe --no-check-certificate 
+wget -P "d:/folder" https://download.geo.drweb.com/pub/drweb/cureit/setup.exe --no-check-certificate 
 :: качаем файл с сайта в папку указанную пользователем
-start d:/folder\setup.exe 
+start "d:/folder\setup.exe" 
 :: запускаем файл 
 exit
