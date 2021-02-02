21758de25d9c6b0b813df2ff156928bf27344af2
BabushkaVoytenko <serezha845765@gmail.com>
2021-02-02 12:04:19 +0300

    create ps1 files

--- WdAdminStartBackup/mailFalse.ps1
+++ WdAdminStartBackup/mailFalse.ps1
@@ -5,10 +5,10 @@ $To = "mail address2" # кому
 $user = "mail address1"
 $pass = "password"
 
-$file = "E:\log.txt" # путь до файла (если нужно прекрипить файл)
+$file = "E:\log.txt" # путь до файла (закомментировать, если не нужен файл)
 $subject = "BACKUP FALSED" # Тема письма
 # Создание экземпляров класса 
-$att = New-object Net.Mail.Attachment($file) # разкоменнтировать если нужен файл
+$att = New-object Net.Mail.Attachment($file) # закомментировать, если не нужен файл
 $mes = New-Object System.Net.Mail.MailMessage
 # Формирование данных для отправки
 $mes.From = $From
@@ -16,15 +16,14 @@ $mes.To.Add($To)
 $mes.Subject = $subject
 $mes.IsBodyHTML = $true
 $mes.Body = "Backup was not created on YandexDisk. Check the log.txt and correct script"
-
 $mes.Attachments.Add($att) # добавление файла (разкоменнтировать если требуется файл)
+
 $smtp = New-Object Net.Mail.SmtpClient($serverSmtp) # создаем экземпляр класса для подключения к SMTP серверу
 $smtp.EnableSSL = $true # сервер использует SSL
 $smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass); #Создаем экземпляр класса для авторизации на сервере
 # отправляем письмо, особождаем память
 $smtp.Send($mes)
-$att.Dispose()
-# данный скрипт взял с сайта https://bezramok-tlt.ru/
+$att.Dispose() # закомментировать, если не нужен файл
 
 
 
