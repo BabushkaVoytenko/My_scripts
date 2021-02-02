bf4711dcf6c4190650d7763e858ab3a6beaed11c
BabushkaVoytenko <serezha845765@gmail.com>
2021-02-02 23:08:34 +0300

    модифицировал скрипт

--- WdAdminStartBackup/WbAdminStartBackup.bat
+++ WdAdminStartBackup/WbAdminStartBackup.bat
@@ -1,65 +1,50 @@
-:: Этот скрипт удаляет файлы из папки загрузки, делает образ системы, копирует его на диск Е
-:: удаляет старые файлы образа, проверяет количество файлов и отправляет log's на почту
-:: с помощью программ mail.ps1 и mailFalse.ps1
+:: Этот скрипт удаляет файлы из папки загрузки, делает образ системы, копирует его на диск Е.
+:: Затем файлы образа перемещаются в папку Яндекс диска и синхронизируются в облако.
+:: Удаляет старые файлы образа,отправляет оповещение и файл логов на почту с помощью mail.ps1.
 @echo off
 Echo System backup in progress. Do not turn off the computer!
-del "d:\Downloads\*.*" /s /f /q >>e:\log.txt 2>&1
 ::удаляем файлы в папке "загрузки"
-for /d %%i in ("d:\Downloads\*") do rmdir /s /q "%%i" >>e:\log.txt 2>&1
+del "d:\Downloads\*.*" /s /f /q >>e:\log.txt 2>&1
 ::команда удаляет подкаталоги в папке "загрузки"
+for /d %%i in ("d:\Downloads\*") do rmdir /s /q "%%i" >>e:\log.txt 2>&1
 E:
-wbAdmin start backup -backupTarget:E: -include:C:,D: -allCritical -quiet >>e:\log.txt 2>&1
 :: Делаем образ дисков "С" и "D" на диск "E"
+wbAdmin start backup -backupTarget:E: -include:C:,D: -allCritical -quiet >>e:\log.txt 2>&1
+:: устанавливаем переменные на директории
 set folder1=E:\WindowsImageBackup\computer_name
 set folder2=E:\YandexDisk\WindowsImageBackup\computer_name
-:: устанавливаем переменные на директории
+:: Данное условие проверяет наличие директории
 if exist %folder1% (
 goto backup
 ) else (
-goto mail1
+goto mail
 )
-:: Данное условие проверяет наличие директории
+
 :backup
+:: копируем все папки и их содержимое в папку Яндекс диска
 xcopy "%folder1%" "%folder2%" /y /e >>e:\log.txt 2>&1
-:: копируем все папки и их содержимое
-forfiles /p "%folder2%" /s /c "cmd /c rd /s /q @file" /d -1 >>e:\log.txt 2>&1
 :: удаляем все файлы измененные день назад и ранее
-set /A "COUNT=0", "COUNT1=0"
+forfiles /p "%folder2%" /s /c "cmd /c rd /s /q @file" /d -1 >>e:\log.txt 2>&1
 :: создаем два выражения для сравнения количества файлов
+set /A "COUNT=0", "COUNT1=0"
+:: считаем количество файлов в только что созданном бэкапе возраст которых не превышет 0 дней (18 файлов)
 for /F %%F in ('
 	robocopy "%folder1%" "%folder1%" /L /E /IS /NP /NS /NC /NJH /NJS /MINAGE:0 /NDL /XD SPPMetadataCache Logs
 ') do set /A "COUNT+=1"
-:: считаем количество файлов в только что созданном бэкапе возраст которых не превышет 0 дней (18 файлов)
+:: считаем количество файлов на Яндекс диске возраст которых не превышает 0 дней (18 файлов)
 for /F %%N in ('
 	robocopy "%folder2%" "%folder2%" /L /E /IS /NP /NS /NC /NJH /NJS /MINAGE:0 /NDL /XD SPPMetadataCache Logs
 ') do set /A "COUNT1+=1"
-:: считаем количество файлов в старом бэкапе на Яндекс диске возраст которых не меньше 30 дней (18 файлов)
-if %COUNT%==%COUNT1% (
-echo %COUNT% %COUNT1% Backup successfully created >>e:\log.txt 2>&1
+:: отправляем результаты в лог и файл count.txt для их сравнения и формирование письма
+echo %COUNT% %COUNT1% Backup successfully created >>e:\log.txt 2>&1 
+echo %COUNT% >>e:\count.txt 2>&1
+echo %COUNT1% >>e:\count1.txt 2>&1
 goto mail
-) else (
-echo %COUNT% %COUNT1% Backup Falsed >>e:\log.txt 2>&1
-goto mail1
-)
+
 :mail
 d:
-cd "d:\folder\WdAdminStartBackup\" 
 ::переходим в папку со скриптом отправки оповещения
+cd "d:\folder\WdAdminStartBackup\" 
+::запускаем скрипт оповещения
 powershell -ExecutionPolicy Bypass -File mail.ps1 
-::запускаем скрипт оповещения
-rmdir /s /q E:\WindowsImageBackup >>e:\log.txt 2>&1
-:: удаляем каталог "WindowsImageBackup" и его подкаталоги в директории "E:\"
-erase E:\*.txt
-:: удаляем txt файл из каталога Е
-exit
-:mail1
-d:
-cd "d:\folder\WdAdminStartBackup\" 
-::переходим в папку со скриптом отправки оповещения
-powershell -ExecutionPolicy Bypass -File mailFalse.ps1 
-::запускаем скрипт оповещения
-rmdir /s /q E:\WindowsImageBackup >>e:\log.txt 2>&1
-:: удаляем каталог "WindowsImageBackup" и его подкаталоги в директории "E:\"
-erase E:\*.txt
-:: удаляем txt файл из каталога Е
 exit
--- WdAdminStartBackup/mail.ps1
+++ WdAdminStartBackup/mail.ps1
@@ -1,29 +1,85 @@
 $serverSmtp = "smtp.mail.ru" # адрес сервера SMTP для отправки
-$From = "mail address1" # от кого
+$From = "System notification <mail address1>" # от кого
 $To = "mail address2" # кому
 # Логин и пароль от ящика с которого отправляю
-$user = "mail address1"
+$user = "mail address"
 $pass = "password"
-
+#--------------------------------------------------------------
 $file = "E:\log.txt" # путь до файла (закомментировать, если не нужен файл)
 $subject = "BACKUP CREATED" # Тема письма
+$subjectfalse = "BACKUP FALSED" # Тема письма если ошибка
 # Создание экземпляров класса 
 $att = New-object Net.Mail.Attachment($file) # закомментировать, если не нужен файл
 $mes = New-Object System.Net.Mail.MailMessage
-# Формирование данных для отправки
-$mes.From = $From
-$mes.To.Add($To)
-$mes.Subject = $subject
-$mes.IsBodyHTML = $true
-$mes.Body = "Backup successfully created on YandexDisk"
-$mes.Attachments.Add($att) # добавление файла (закомментировать, если не нужен файл)
+#--------------------------------------------------------------
+# устанавливаем переменные на директории, которую нужно проверить
+$path= Test-Path "E:\WindowsImageBackup\computer_name"
+# Данное условие проверяет наличие директории
+if ($path -eq $True) { 
+	# сравниваем количество файлов выведеных в файл count.txt
+	$count = Get-Content E:\count.txt | Select-Object -Index 0 # извлекаем первую строку из лога 
+	$count = [int]$count # преобразуем первую строку в число
+	$count1 = Get-Content E:\count.txt | Select-Object -Index 1 # извлекаем вторую строку из лога
+	$count1 = [int]$count1 # преобразуем вторую строку в число
+	# сравниваем числа, если они совпадают письмо об успешной отправке
+	# если числа не совпадают, формируем письмо об ошибке
+	#-------------------------------------------------------------
+	If ($count -eq $count1) { 
+		# Формирование данных для отправки
+		$mes.From = $From
+		$mes.To.Add($To)
+		$mes.Subject = $subject
+		$mes.IsBodyHTML = $true
+		$mes.Body = "Backup successfully created on YandexDisk"
+		$mes.Attachments.Add($att) # добавление файла (закомментировать, если не нужен файл)
+		#---------------------------------------------------------
+		$smtp = New-Object Net.Mail.SmtpClient($serverSmtp) # создаем экземпляр класса для подключения к SMTP серверу
+		$smtp.EnableSSL = $true # сервер использует SSL
+		$smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass); #Создаем экземпляр класса для авторизации на сервере
+		# отправляем письмо, особождаем память
+		$smtp.Send($mes)
+		$att.Dispose()
+	}
+	else {
+		$mes.From = $From
+		$mes.To.Add($To)
+		$mes.Subject = $subjectfalse
+		$mes.IsBodyHTML = $true
+		$mes.Body = "Backup was not created on YandexDisk. Check the log.txt and correct script"
+		$mes.Attachments.Add($att) # добавление файла (закомментировать, если не нужен файл)
+		#---------------------------------------------------------
+		$smtp = New-Object Net.Mail.SmtpClient($serverSmtp) # создаем экземпляр класса для подключения к SMTP серверу
+		$smtp.EnableSSL = $true # сервер использует SSL
+		$smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass); #Создаем экземпляр класса для авторизации на сервере
+		# отправляем письмо, особождаем память
+		$smtp.Send($mes)
+		$att.Dispose()
+	}
+	# удаляем каталог "WindowsImageBackup" и его подкаталоги в директории "E:\"
+	Remove-Item -recurse E:\WindowsImageBackup
+	# удаляем txt файл из каталога Е
+	Remove-Item E:\*.txt
+	Exit	
+}
+else {
+	$mes.From = $From
+	$mes.To.Add($To)
+	$mes.Subject = $subjectfalse
+	$mes.IsBodyHTML = $true
+	$mes.Body = "Backup was not created on YandexDisk. Check the log.txt and correct script"
+	$mes.Attachments.Add($att) # добавление файла (закомментировать, если не нужен файл)
+	#---------------------------------------------------------
+	$smtp = New-Object Net.Mail.SmtpClient($serverSmtp) # создаем экземпляр класса для подключения к SMTP серверу
+	$smtp.EnableSSL = $true # сервер использует SSL
+	$smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass); #Создаем экземпляр класса для авторизации на сервере
+	# отправляем письмо, особождаем память
+	$smtp.Send($mes)
+	$att.Dispose()
+}
 
-$smtp = New-Object Net.Mail.SmtpClient($serverSmtp) # создаем экземпляр класса для подключения к SMTP серверу
-$smtp.EnableSSL = $true # сервер использует SSL
-$smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass); #Создаем экземпляр класса для авторизации на сервере
-# отправляем письмо, особождаем память
-$smtp.Send($mes)
-$att.Dispose() # закомментировать, если не нужен файл
+
+
+
 
 
 
