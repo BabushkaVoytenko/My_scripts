0d4608233342d37f8f9cc2b67380aa8b48f4c9e4
BabushkaVoytenko <serezha845765@gmail.com>
2021-04-18 16:10:06 +0300

    кодировка utf-8

--- WbAdminStartBackupV2.ps1
+++ WbAdminStartBackupV2.ps1
@@ -1,45 +1,45 @@
 #-----------------------------------------------------------------------------------------------
-#Этот скрипт делает образ системы, копирует его на Яндекс диск.
-#Отправляет оповещение в телеграм бот (который нужно создать).Удаляет предыдущий образ системы,
-#логи с диска и старый образ из корзины Яндекс диска.
+#Р­С‚РѕС‚ СЃРєСЂРёРїС‚ РґРµР»Р°РµС‚ РѕР±СЂР°Р· СЃРёСЃС‚РµРјС‹, РєРѕРїРёСЂСѓРµС‚ РµРіРѕ РЅР° РЇРЅРґРµРєСЃ РґРёСЃРє.
+#РћС‚РїСЂР°РІР»СЏРµС‚ РѕРїРѕРІРµС‰РµРЅРёРµ РІ С‚РµР»РµРіСЂР°Рј Р±РѕС‚ (РєРѕС‚РѕСЂС‹Р№ РЅСѓР¶РЅРѕ СЃРѕР·РґР°С‚СЊ).РЈРґР°Р»СЏРµС‚ РїСЂРµРґС‹РґСѓС‰РёР№ РѕР±СЂР°Р· СЃРёСЃС‚РµРјС‹,
+#Р»РѕРіРё СЃ РґРёСЃРєР° Рё СЃС‚Р°СЂС‹Р№ РѕР±СЂР°Р· РёР· РєРѕСЂР·РёРЅС‹ РЇРЅРґРµРєСЃ РґРёСЃРєР°.
 #-----------------------------------------------------------------------------------------------
-#Всплывающее уведомление для пользователя
+#Р’СЃРїР»С‹РІР°СЋС‰РµРµ СѓРІРµРґРѕРјР»РµРЅРёРµ РґР»СЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ
 Add-Type -AssemblyName System.Windows.Forms
 $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
 $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
 $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
-$balmsg.BalloonTipText = 'Осуществляется бэкап системы! Не выключайте компьютер!'
-$balmsg.BalloonTipTitle = "Внимание $Env:USERNAME"
+$balmsg.BalloonTipText = 'РћСЃСѓС‰РµСЃС‚РІР»СЏРµС‚СЃСЏ Р±СЌРєР°Рї СЃРёСЃС‚РµРјС‹! РќРµ РІС‹РєР»СЋС‡Р°Р№С‚Рµ РєРѕРјРїСЊСЋС‚РµСЂ!'
+$balmsg.BalloonTipTitle = "Р’РЅРёРјР°РЅРёРµ $Env:USERNAME"
 $balmsg.Visible = $true
 $balmsg.ShowBalloonTip(10000)
 #----------------------------------------------------------------------------------------------
-#Открыл папку для общего доступа, потому что у wbadmin.exe нет другого способа создать образ системы 
-#в конкретном месте. Делаем образ дисков "С" и "D" на диск "E" в папку YandexDisk
+#РћС‚РєСЂС‹Р» РїР°РїРєСѓ РґР»СЏ РѕР±С‰РµРіРѕ РґРѕСЃС‚СѓРїР°, РїРѕС‚РѕРјСѓ С‡С‚Рѕ Сѓ wbadmin.exe РЅРµС‚ РґСЂСѓРіРѕРіРѕ СЃРїРѕСЃРѕР±Р° СЃРѕР·РґР°С‚СЊ РѕР±СЂР°Р· СЃРёСЃС‚РµРјС‹ 
+#РІ РєРѕРЅРєСЂРµС‚РЅРѕРј РјРµСЃС‚Рµ. Р”РµР»Р°РµРј РѕР±СЂР°Р· РґРёСЃРєРѕРІ "РЎ" Рё "D" РЅР° РґРёСЃРє "E" РІ РїР°РїРєСѓ YandexDisk
 wbAdmin.exe start backup -backupTarget:\\computer_name\YandexDisk\ -allCritical -quiet >>e:\log.txt 2>&1
-#находим фразу в log.txt, которая свидетельствует об успешном создании бэкапа
-$string = 'Архивация успешно завершена.'
+#РЅР°С…РѕРґРёРј С„СЂР°Р·Сѓ РІ log.txt, РєРѕС‚РѕСЂР°СЏ СЃРІРёРґРµС‚РµР»СЊСЃС‚РІСѓРµС‚ РѕР± СѓСЃРїРµС€РЅРѕРј СЃРѕР·РґР°РЅРёРё Р±СЌРєР°РїР°
+$string = 'РђСЂС…РёРІР°С†РёСЏ СѓСЃРїРµС€РЅРѕ Р·Р°РІРµСЂС€РµРЅР°.'
 $content = get-content e:\log.txt
 $string2 = $content | select-string -Pattern $string -Quiet -encoding oem
-# создаем условие. Если фраза существует отправляем сообщение об успешном бэкапе.
-# Если фразы не существует отправляем log.txt для проверки.
+# СЃРѕР·РґР°РµРј СѓСЃР»РѕРІРёРµ. Р•СЃР»Рё С„СЂР°Р·Р° СЃСѓС‰РµСЃС‚РІСѓРµС‚ РѕС‚РїСЂР°РІР»СЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ РѕР± СѓСЃРїРµС€РЅРѕРј Р±СЌРєР°РїРµ.
+# Р•СЃР»Рё С„СЂР°Р·С‹ РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚ РѕС‚РїСЂР°РІР»СЏРµРј log.txt РґР»СЏ РїСЂРѕРІРµСЂРєРё.
 if($string2 -eq $True){
 curl.exe -s -X POST `
 https://api.telegram.org/bot<TOKEN>/sendMessage `
--d chat_id=<ID_ЧАТА> -d text="Backup successfully created on YandexDisk)))" | Out-Null
+-d chat_id=<ID_Р§РђРўРђ> -d text="Backup successfully created on YandexDisk)))" | Out-Null
 Remove-Item e:\log.txt
-# очищаем корзину яндекс диска
-curl.exe -s -H "Authorization: OAuth ваштокен" -X "DELETE" `
+# РѕС‡РёС‰Р°РµРј РєРѕСЂР·РёРЅСѓ СЏРЅРґРµРєСЃ РґРёСЃРєР°
+curl.exe -s -H "Authorization: OAuth РІР°С€С‚РѕРєРµРЅ" -X "DELETE" `
 https://cloud-api.yandex.net/v1/disk/trash/resources/?path=
-# узнать свой токен: https://oauth.yandex.ru/authorize?response_type=token&display=popup&client_id=вашid
+# СѓР·РЅР°С‚СЊ СЃРІРѕР№ С‚РѕРєРµРЅ: https://oauth.yandex.ru/authorize?response_type=token&display=popup&client_id=РІР°С€id
 Exit	
 }
 else {
 curl.exe -s -X POST `
 https://api.telegram.org/bot<TOKEN>/sendMessage `
--d chat_id=<ID_ЧАТА> -d text="Backup was not created on YandexDisk(((. `
+-d chat_id=<ID_Р§РђРўРђ> -d text="Backup was not created on YandexDisk(((. `
 Check the log.txt" | Out-Null
-curl.exe -F chat_id=<ID_ЧАТА> -F document=@E:\log.txt `
+curl.exe -F chat_id=<ID_Р§РђРўРђ> -F document=@E:\log.txt `
 https://api.telegram.org/bot<TOKEN>/sendDocument | Out-Null
 Exit
 }
